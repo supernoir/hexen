819007b459bea76740cabe6faf77e50ede154e92
supernoir <kawohl.bentodesign@gmail.com>
2017-06-25 23:58:06 +0200

    iterates over data from hn, accumulates titles

--- index.js
+++ index.js
@@ -4,23 +4,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+const topstories = [];
+let accumulatedStories = [];
+
 class Hexen extends React.Component {
   constructor() {
     super();
+    this.state = {
+      news: [],
+    };
   }
 
   componentWillMount() {
     axios
       .get('https://hacker-news.firebaseio.com/v0/topstories.json')
       .then(res => {
-        console.log(res);
+        for (let i = 0; i < 10; i++) {
+          const newsItem = res.data[i];
+          topstories.push(newsItem);
+        }
+        for (const topstory of topstories) {
+          console.log(topstory);
+          axios
+            .get(
+              'https://hacker-news.firebaseio.com/v0/item/' + topstory + '.json'
+            )
+            .then(res => {
+              let newsTitle = res.data.title;
+              accumulatedStories.push(newsTitle);
+              //console.log(accumulatedStories);
+            });
+        }
       });
+
+    this.setState({accumulatedStories});
+    console.log(accumulatedStories);
   }
 
   render() {
     return (
       <div>
         <h1>Hexen</h1>
+        {this.state.news.map(n => (
+          <li>
+            {n}
+          </li>
+        ))}
       </div>
     );
   }
