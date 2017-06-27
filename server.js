59de1b46c270ac351af3071f1d8f8f6bfaea580a
Conny Kawohl <conny.kawohl@useeds.de>
2017-06-27 16:58:00 +0200

    accumulates hn data into new route

--- server.js
+++ server.js
@@ -1,6 +1,67 @@
 const express = require('express')
 const app = express()
 const port = 5555 || process.env.port
+const axios = require('axios')
+
+const topstories = []
+let accumulatedStories = {}
+const accumulatedTitles = []
+const accumulatedEditors = []
+const accumulatedIDs = []
+const accumulatedScores = []
+const accumulatedURIs = []
+
+const getAllTopStories = () => {
+  const fetchTopStories = new Promise((resolve, reject) => {
+    axios
+      .get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(allStories => {
+        for (let i = 0; i < 9; i++) {
+          const newsItem = allStories.data[i]
+          topstories.push(newsItem)
+        }
+        for (const topstory of topstories) {
+          axios
+            .get(
+              'https://hacker-news.firebaseio.com/v0/item/' + topstory + '.json'
+            )
+            .then(stories => {
+              let newsID = Math.ceil(Math.random(10))
+              let newsTitle = stories.data.title
+              let newsEditor = stories.data.by
+              let newsScore = stories.data.score
+              let newsURI = stories.data.url
+
+              accumulatedIDs.push({ id: newsID })
+              accumulatedTitles.push({ title: newsTitle })
+              accumulatedEditors.push({ editor: newsEditor })
+              accumulatedScores.push({ score: newsScore })
+              accumulatedURIs.push({ uri: newsURI })
+
+              accumulatedStories = {
+                stories: {
+                  ids: accumulatedIDs,
+                  titles: accumulatedTitles,
+                  editors: accumulatedEditors,
+                  scores: accumulatedScores,
+                  uris: accumulatedURIs
+                }
+              }
+
+              resolve(accumulatedStories)
+            })
+        }
+      })
+  })
+
+  fetchTopStories.then(data => {
+    app.get('/topstories', (req, res) => {
+      res.json({ hi: data })
+    })
+  })
+}
+
+getAllTopStories()
 
 const fakeApiCall = [
   { id: 0, title: 'Lorem Ipsum', editor: 'foo', uri: 'https://spiegel.de' },
